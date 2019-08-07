bff13631dc018cf74ac383b8f5246d540c663fcb
angelineemi <48149076+angelineemi@users.noreply.github.com>
2019-08-07 18:11:46 +0530

    Create oddnumlist1.py

---
+++ oddnumlist1.py
@@ -0,0 +1,9 @@
+str = str (input("Enter comma separated integers: "))
+list = str.split (",")
+li = []
+for i in list:
+	li.append(int(i))
+for dig in li:
+    if dig%2!=0:
+        print(dig)
+                
